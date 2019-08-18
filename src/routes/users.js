84f86913cf9bec70b8c8de69b2697e60b41594c2
Ignacio <shoik.xtazy@gmail.com>
2019-08-17 22:15:30 -0400

    added error message in login when form is incompleted

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,8 +8,30 @@ router.get('/user/signup', (req,res) => {
 });
 
 router.post('/user/signup',(req,res) => {
-    res.send("scaning");
-    console.log(req.body);
+    const {username,email,password,password_confirm} = req.body;
+
+    const errors = [];
+
+    if(username.length <= 0 ){
+		errors.push({text:'Please Typed a Username'});
+	}
+
+	if(password != password_confirm){
+		errors.push({text:'Password do not match'});
+    }
+    
+    if(email.length <= 0){
+		errors.push({text:'Please Typed an Email'});
+	}
+
+	if(password.length < 4){
+		errors.push({text: 'Password must be at least 4 characters'});
+    }
+    
+    if(errors.length > 0){
+        res.render('users/signup',{errors,username,email,password,password_confirm});
+    }
+    
 });
 
  module.exports = router;
